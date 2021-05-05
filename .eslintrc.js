68d4d58bd5de64cf45ae7c686d6622870c7bc88a
Gabriel Do Couto <gabriel.couto@nave.rs>
2021-05-04 23:22:37 -0300

    prettier file

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/react-in-jsx-scope": "off",
+    semi: 0,
+    "@typescript-eslint/semi": ["error"],
+    "@typescript-eslint/no-non-null-assertion": "off",
+  },
+};
